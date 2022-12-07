3e06c6d891ac8e2fd1af056a18e67e206594c0e9
Jack Hurley <hurleyuv@bc.edu>
2022-12-07 11:20:54 -0500

    Initial Commit

---
+++ MedConnect/MedConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedConnectApp.swift
+//  MedConnect
+//
+//  Created by Jack Hurley on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MedConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
